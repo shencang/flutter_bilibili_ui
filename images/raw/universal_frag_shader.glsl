4a1e8ce7f41e38dbfd15aba3d4cf29afaf530143
shencang <2630610009@qq.com>
2019-05-28 15:55:14 +0800

    删除一些多余文件

--- images/raw/universal_frag_shader.glsl
+++
@@ -1,8 +0,0 @@
-uniform sampler2D inputImageTexture;
-varying highp vec2 textureCoordinate;
-
-void main()
-{
-    lowp vec4 color = texture2D(inputImageTexture, textureCoordinate);
-    gl_FragColor = color;
-}
--- images/raw/universal_vertex_shader.glsl
+++
@@ -1,9 +0,0 @@
-attribute highp vec2 posAttr;
-attribute highp vec2 texCoordAttr;
-varying highp vec2 textureCoordinate;
-
-void main()
-{
-    textureCoordinate = texCoordAttr;
-    gl_Position = vec4(posAttr, 0, 1);
-}
